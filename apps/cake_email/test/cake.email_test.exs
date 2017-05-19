adad54bde774ca0f465830c7868946af1b59a7fd
Stefan Johnson <sj@scrimpycat.io>
2017-05-20 01:20:29 +1000

    Test composing emails

--- apps/cake_email/test/cake.email_test.exs
+++ apps/cake_email/test/cake.email_test.exs
@@ -1,4 +1,36 @@
 defmodule Cake.EmailTest do
-  use ExUnit.Case
-  doctest Cake.Email
+    use ExUnit.Case
+    alias Cake.Email
+    doctest Email
+
+    defmodule TestTemplate do
+        defstruct [
+            formatter: &TestTemplate.format/1,
+            name: nil,
+            message: nil
+        ]
+
+        def format(%{ name: name, message: message }) do
+            %Email{
+                subject: "A test message",
+                body: %Email.Body{
+                    text: "Hi #{name}, #{message}."
+                }
+            }
+        end
+    end
+
+    test "custom template" do
+        assert %Email{
+            subject: "A test message",
+            body: %Email.Body{ text: "Hi Foo, Bar." }
+        } == Email.compose(%TestTemplate{ name: "Foo", message: "Bar" })
+    end
+
+    test "overriding fields" do
+        assert %Email{
+            subject: "test",
+            body: %Email.Body{ text: "Hi Foo, Bar." }
+        } == Email.compose(%TestTemplate{ name: "Foo", message: "Bar" }, subject: "test")
+    end
 end
