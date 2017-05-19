71871388c80045511725f839cebbf8761c7183ea
Stefan Johnson <sj@scrimpycat.io>
2017-05-20 00:42:43 +1000

    Document email struct and composition

--- apps/cake_email/lib/cake.email.ex
+++ apps/cake_email/lib/cake.email.ex
@@ -1,4 +1,36 @@
 defmodule Cake.Email do
+    @moduledoc """
+      Compose and represent emails.
+
+      ##Fields
+
+      ###:from
+      The email address of the sender.
+
+      ###:to
+      The email address for the recipient(s).
+
+      ###:reply_to
+      The email address to direct replies to.
+
+      ###:headers
+      Any headers to include with the email.
+
+      ###:cc
+      The email address for any recipient(s) to CC to.
+
+      ###:bcc
+      The email address of any recipient(s) to BCC to.
+
+      ###:subject
+      The subject title of the email.
+
+      ###:body
+      The content of the email.
+
+      ###:attachments
+      The file attachments included in the email.
+    """
     alias Cake.Email
 
     @type address :: { name :: String.t, email :: String.t } | email :: String.t
@@ -28,6 +60,33 @@ defmodule Cake.Email do
         :attachments
     ]
 
+    @doc """
+      Create an email from the given input.
+
+      Emails can be composed from pre-existing emails and overriding certain fields. Or
+      from constructing them from templates. A valid email template only needs to be a
+      map consisting of a `:formatter` field with a function that accepts that map and
+      returns an email.
+
+      A common approach for templates is to use them for building commonly structured
+      emails.
+
+        iex> template = %Cake.Email.Template{
+        ...>    formatter: fn %{ data: name } ->
+        ...>        %Cake.Email{
+        ...>            subject: "A message to \#{name}",
+        ...>            body: %Cake.Email.Body{
+        ...>                text: "Hello \#{name}!"
+        ...>            }
+        ...>        }
+        ...>    end,
+        ...>    data: "Blah"
+        ...> }
+        iex> Cake.Email.compose(template)
+        %Cake.Email{attachments: nil, bcc: nil, body: %Cake.Email.Body{html: nil, text: "Hello Blah!"}, cc: nil, from: nil, headers: nil, reply_to: nil, subject: "A message to Blah", to: nil}
+        iex> Cake.Email.compose(template, to: "foo@bar", subject: "Overridden")
+        %Cake.Email{attachments: nil, bcc: nil, body: %Cake.Email.Body{html: nil, text: "Hello Blah!"}, cc: nil, from: nil, headers: nil, reply_to: nil, subject: "Overridden", to: "foo@bar"}
+    """
     @spec compose(t | template, keyword) :: t
     def compose(email, params \\ [])
     def compose(email = %Email{}, params), do: Map.merge(email, Map.new(params))
