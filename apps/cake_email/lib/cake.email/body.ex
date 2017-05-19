7a10cad28643081b82218b5700fb3c8292d3d033
Stefan Johnson <sj@scrimpycat.io>
2017-05-20 00:20:04 +1000

    Document body struct

--- apps/cake_email/lib/cake.email/body.ex
+++ apps/cake_email/lib/cake.email/body.ex
@@ -1,4 +1,16 @@
 defmodule Cake.Email.Body do
+    @moduledoc """
+      The email's body representation.
+
+      ##Fields
+
+      ###:text
+      The text version of the email's body.
+
+      ###:html
+      The HTML version of the email's body.
+
+    """
     alias Cake.Email.Body
 
     @type t :: %Body{ text: String.t | nil, html: String.t | nil }
