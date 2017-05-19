65d375608d47955e84f8c66c6f45a869d35f8646
Stefan Johnson <sj@scrimpycat.io>
2017-05-19 23:21:46 +1000

    Document template struct

--- apps/cake_email/lib/cake.email/template.ex
+++ apps/cake_email/lib/cake.email/template.ex
@@ -1,4 +1,30 @@
 defmodule Cake.Email.Template do
+    @moduledoc """
+      A simple generic email template to build emails from.
+
+      ##Fields
+
+      ###:formatter
+      A function that will accept the template and return an email.
+
+      ###:data
+      Any additional data you want to access from the formatter function.
+
+      ##Example
+
+        Cake.Email.compose(%Cake.Email.Template{
+            formatter: fn %{ data: name } ->
+                %Cake.Email{
+                    subject: "A message to \#{name}",
+                    body: %Cake.Email.Body{
+                        text: "Hello \#{name}!"
+                    }
+                }
+            end,
+            data: "Blah"
+        })
+        \#=> %Cake.Email{attachments: nil, bcc: nil, body: %Cake.Email.Body{html: nil, text: "Hello Blah!"}, cc: nil, from: nil, headers: nil, reply_to: nil, subject: "A message to Blah", to: nil}
+    """
     alias Cake.Email
     alias Cake.Email.Template
 
